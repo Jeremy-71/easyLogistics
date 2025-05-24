728b6395fd3df8368f73cd3efc7c99c68688b1a8
remy bu <ermiasbulcha93@gmail.com>
2025-05-24 20:43:17 +0300

    i have changed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import  Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <main className="p-6">
+      <h1 className="text-xl font-bold mb-4">Product List</h1>
+      <Sidebar />
+    </main>
+  );
+}
+
+export default App;
